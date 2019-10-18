d47ae1135fe4fe7e492b1ecff44e11e41672779b
Jakub Liput <jakub.liput@gmail.com>
2019-10-18 10:38:34 +0200

    VFS-5651 PR fixes

--- src/app/services/store.js
+++ src/app/services/store.js
@@ -1,10 +1,11 @@
 import OnedataWebsocketStore from 'onedata-gui-websocket-client/services/store';
+import { entityType as userEntityType } from 'oneprovider-gui/models/user';
 
 export default OnedataWebsocketStore.extend({
   /**
    * @override 
    */
-  userEntityType: 'op_user',
+  userEntityType,
 
   /**
    * @override 
