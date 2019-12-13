bf0634248fec37622232bf7de94bd37b4bb27f75
Jakub Liput <jakub.liput@gmail.com>
2019-12-13 15:44:12 +0100

    VFS-5781 After-presentation fixes

--- src/app/components/db-view-modal.js
+++ src/app/components/db-view-modal.js
@@ -12,10 +12,11 @@ import I18n from 'onedata-gui-common/mixins/components/i18n';
 import { reads } from '@ember/object/computed';
 import { get, getProperties, computed } from '@ember/object';
 import gri from 'onedata-gui-websocket-client/utils/gri';
-import { providerEntityType } from 'oneprovider-gui/models/provider';
+import { entityType as providerEntityType } from 'oneprovider-gui/models/provider';
 import { inject as service } from '@ember/service';
 import { promise } from 'ember-awesome-macros';
 import { notImplementedThrow } from 'onedata-gui-common/utils/not-implemented-throw';
+import { all as allSettled } from 'rsvp';
 
 export default Component.extend(I18n, {
   /**
@@ -89,16 +90,17 @@ export default Component.extend(I18n, {
       const store = this.get('store');
       return this.get('dbViewProxy').then((dbView) => {
         const providers = get(dbView, 'providers');
-        const providersGris = providers.map((providerId) => gri({
-          entityType: providerEntityType,
-          entityId: providerId,
-          aspect: 'instance',
-          scope: 'private',
+        return allSettled(providers.map((providerId) => {
+          const providerGri = gri({
+            entityType: providerEntityType,
+            entityId: providerId,
+            aspect: 'instance',
+            scope: 'protected',
+          });
+          return store.findRecord('provider', providerGri)
+            .then(provider => get(provider, 'name'))
+            .catch(() => providerId);
         }));
-        return providersGris.map(gri =>
-          store.findRecord('provider', gri)
-          .then(providers => providers.mapBy('name'))
-        );
       });
     }),
   ),
