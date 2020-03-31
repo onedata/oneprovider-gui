84b507ab32d8b33bd1589c9c5bf216827c917c5a
Jakub Liput <jakub.liput@gmail.com>
2020-03-31 19:20:29 +0200

    VFS-6145 Draft styles for qos expressions

--- src/app/components/qos-expression-viewer.js
+++ src/app/components/qos-expression-viewer.js
@@ -3,6 +3,8 @@ import qosRpnToObject from 'oneprovider-gui/utils/qos-rpn-to-object';
 import computedPipe from 'onedata-gui-common/utils/ember/computed-pipe';
 
 export default Component.extend({
+  classNames: ['qos-expression-viewer'],
+
   expression: undefined,
 
   data: computedPipe('expression', qosRpnToObject),
