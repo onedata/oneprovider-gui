cf4d7e7bc904a6f1b2a24766d9646e46b02349ac
Jakub Liput <jakub.liput@gmail.com>
2020-12-17 19:42:08 +0100

    VFS-6802 PR fixes: moved translations

--- src/app/locales/en/components/query-builder.js
+++ src/app/locales/en/components/query-builder.js
@@ -1,7 +1,24 @@
+const comparators = {
+  provider: {
+    is: 'is',
+  },
+  storage: {
+    is: 'is',
+  },
+};
+
 export default {
   valueEditors: {
     dropdownEditor: {
       providedBy: 'provided by',
     },
+  },
+  blockSelector: {
+    conditionSelector: {
+      comparators,
+    },
+  },
+  conditionBlock: {
+    comparators,
   },
 };
