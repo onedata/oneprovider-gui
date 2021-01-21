af185ef9c1b08d61e8a230950610fd00f8894b3c
plgagneska <agawarchol15@gmail.com>
2021-01-21 13:12:54 +0100

    VFS-6915 Global copy public url in shares

--- src/app/locales/en/components/space-shares/list-item.js
+++ src/app/locales/en/components/space-shares/list-item.js
@@ -2,8 +2,7 @@ export default {
   removeShare: 'Remove share',
   rename: 'Rename',
   copyPublicUrl: 'Copy public URL',
-  clipboardSuccess: 'Share public URL has been copied to clipboard',
-  clipboardError: 'Could not copy share public URL to clipboard',
   deletedFileIconTip: 'This share points to a deleted file.',
   deletedDirectoryIconTip: 'This share points to a deleted directory.',
+  publicUrl: 'Share public URL',
 };
