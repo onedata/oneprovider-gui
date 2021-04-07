75154a250dbad41935404281ed86ceeed2284579
Jakub Liput <jakub.liput@gmail.com>
2021-04-08 00:08:32 +0200

    VFS-7362 Fixed metadata modal requests

--- src/app/locales/en/components/content-file-browser.js
+++ src/app/locales/en/components/content-file-browser.js
@@ -1,8 +1,3 @@
 export default {
-  fileType: {
-    file: 'file',
-    dir: 'directory',
-  },
   openingDirectory: 'opening the directory',
-  metadataIsProtected: '{{fileTypeUpper}} metadata is write protected.',
 };
