c3116bc984009e81820293e1e4a8457111345ce9
Jakub Liput <jakub.liput@gmail.com>
2021-05-24 10:19:48 +0200

    VFS-7648 Using custom resolveParent function in breadcrumbs

--- src/app/utils/add-ellipsis-breadcrumbs-item.js
+++ src/app/utils/add-ellipsis-breadcrumbs-item.js
@@ -19,22 +19,24 @@ import { assert } from '@ember/debug';
 import { get } from '@ember/object';
 import { resolve } from 'rsvp';
 import FileBreadcrumbsItem from 'oneprovider-gui/utils/file-breadcrumbs-item';
+import defaultResolveParent from 'oneprovider-gui/utils/default-resolve-parent';
 
 export const ellipsisString = '...';
 
 /**
  * @param {Ember.A<FileBreadcrumbsItem>} items
  * @param {FileBreadcrumbsItem} child
+ * @param {Function} resolveParent
  * @returns {Promise<Ember.A<FileBreadcrumbsItem>>} resolves with reference to altered `items`
  */
-function addEllipsisBreadcrumbsItem(items, child) {
+function addEllipsisBreadcrumbsItem(items, child, resolveParent = defaultResolveParent) {
   assert(
     'utils/add-ellipsis-breadcrumbs-item: child item cannot be null or undefined',
     child
   );
   const hasEllipsisFile = child.get('file.hasParent');
   if (hasEllipsisFile) {
-    return get(child, 'file.parent').then(ellipsisFile => {
+    return resolveParent(get(child, 'file')).then(ellipsisFile => {
       const childIndex = items.indexOf(child);
       const originalParent = items.objectAt(childIndex - 1);
       if (originalParent &&
---
+++ src/app/utils/default-resolve-parent.js
@@ -0,0 +1,18 @@
+/**
+ * Default method for getting browsable object parent
+ *
+ * @module utils/default-resolve-parent
+ * @author Jakub Liput
+ * @copyright (C) 2021 ACK CYFRONET AGH
+ * @license This software is released under the MIT license cited in 'LICENSE.txt'.
+ */
+
+import { get } from '@ember/object';
+
+export default async function defaultResolveParent(item) {
+  if (get(item, 'hasParent')) {
+    return get(item, 'parent');
+  } else {
+    return null;
+  }
+}
