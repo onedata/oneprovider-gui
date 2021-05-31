191c40ad052a56e64dd39a441a29765bbee169fa
Jakub Liput <jakub.liput@gmail.com>
2021-05-31 17:44:50 +0200

    VFS-7649 Moved remove modal classes to common classes

---
+++ src/app/components/dummy-fb-remove-modal.js
@@ -0,0 +1,22 @@
+import Component from '@ember/component';
+import { collect } from 'ember-awesome-macros';
+import { reads } from '@ember/object/computed';
+import { inject as service } from '@ember/service';
+
+export default Component.extend({
+  mockBackend: service(),
+
+  files: collect(
+    'mockBackend.entityRecords.file.10',
+    'mockBackend.entityRecords.file.11',
+    'mockBackend.entityRecords.file.12',
+  ),
+
+  parentDir: reads('mockBackend.entityRecords.spaceRootDir.0'),
+
+  init() {
+    this._super(...arguments);
+    console.log(this.get('mockBackend.entityRecords.file.10'));
+    console.log(this.get('mockBackend.entityRecords.spaceRootDir'));
+  },
+});
