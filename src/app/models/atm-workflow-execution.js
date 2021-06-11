819660b7c75d1ce77d5f49cbc57d94f41aa8f05c
Michał Borzęcki <michal.borzecki@hotmail.com>
2021-06-11 13:57:47 +0200

    VFS-7742 Added basic execution model

--- src/app/models/atm-workflow-execution.js
+++ src/app/models/atm-workflow-execution.js
@@ -6,9 +6,36 @@
  */
 
 import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
+import { belongsTo } from 'onedata-gui-websocket-client/utils/relationships';
 import StaticGraphModelMixin from 'onedata-gui-websocket-client/mixins/models/static-graph-model';
 import GraphSingleModelMixin from 'onedata-gui-websocket-client/mixins/models/graph-single-model';
 
 export const entityType = 'op_atm_workflow_execution';
 
-export default Model.extend(GraphSingleModelMixin, {}).reopenClass(StaticGraphModelMixin);
+export default Model.extend(GraphSingleModelMixin, {
+  /**
+   * @type {ComputedProperty<String>}
+   */
+  status: attr('string'),
+
+  /**
+   * @type {ComputedProperty<Number>}
+   */
+  scheduleTime: attr('number'),
+
+  /**
+   * @type {ComputedProperty<Number>}
+   */
+  startTime: attr('number'),
+
+  /**
+   * @type {ComputedProperty<Number>}
+   */
+  finishTime: attr('number'),
+
+  /**
+   * @type {ComputedProperty<Models.AtmWorkflowSchemaSnapshot>}
+   */
+  schemaSnapshot: belongsTo('atm-workflow-schema-snapshot'),
+}).reopenClass(StaticGraphModelMixin);
