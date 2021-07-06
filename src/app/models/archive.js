b3b9d7afdbe79f41aef7047c798ae29a31a761ca
Jakub Liput <jakub.liput@gmail.com>
2021-07-06 16:15:24 +0200

    VFS-7893 Fixed ESLint

--- src/app/models/archive.js
+++ src/app/models/archive.js
@@ -10,7 +10,6 @@ import attr from 'ember-data/attr';
 import { belongsTo } from 'onedata-gui-websocket-client/utils/relationships';
 import StaticGraphModelMixin from 'onedata-gui-websocket-client/mixins/models/static-graph-model';
 import GraphSingleModelMixin from 'onedata-gui-websocket-client/mixins/models/graph-single-model';
-import { computed } from '@ember/object';
 
 export const entityType = 'op_archive';
 
