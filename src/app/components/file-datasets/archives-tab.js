bc80bb62636e3918ca892a8de3ade2909f0134e9
Jakub Liput <jakub.liput@gmail.com>
2021-10-26 16:22:33 +0200

    VFS-7898 Fixed choosing hardlinks tab in modal in datasets panel browser

--- src/app/components/file-datasets/archives-tab.js
+++ src/app/components/file-datasets/archives-tab.js
@@ -126,6 +126,12 @@ export default Component.extend(...mixins, {
   fileToShowInfo: null,
 
   /**
+   * One of: general, hardlinks.
+   * @type {String}
+   */
+  showInfoInitialTab: undefined,
+
+  /**
    * @type {Models.File}
    */
   fileToShowMetadata: null,
@@ -478,10 +484,12 @@ export default Component.extend(...mixins, {
     this.set('archivesToPurge', null);
   },
 
-  openInfoModal(file) {
-    this.set('fileToShowInfo', file);
+  openInfoModal(file, activeTab) {
+    this.setProperties({
+      fileToShowInfo: file,
+      showInfoInitialTab: activeTab || 'general',
+    });
   },
-
   closeInfoModal() {
     this.set('fileToShowInfo', null);
   },
