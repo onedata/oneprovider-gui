4c67804b34ef20ad89812ddbff3b5faffccaa297
Jakub Liput <jakub.liput@gmail.com>
2021-11-19 08:34:58 +0100

    VFS-7724 Displaying archives browser in splitted view

--- src/app/locales/en/components/content-space-datasets.js
+++ src/app/locales/en/components/content-space-datasets.js
@@ -3,4 +3,7 @@ export default {
   openingDataset: 'opening dataset',
   attached: 'Attached',
   detached: 'Detached',
+  multiDatasetsSelected: '{{count}} datasets selected.',
+  noDatasetSelected: 'No dataset selected.',
+  selectSingleDataset: 'Select a single dataset to browse its archives.',
 };
