3f45da8d0a847874c2104cbe75456002ff9a0e3e
Jakub Liput <jakub.liput@gmail.com>
2022-01-21 18:31:09 +0100

    VFS-8792 Fixed incorrect "1 files" text in archive-recall

--- src/app/locales/en/components/archive-recall.js
+++ src/app/locales/en/components/archive-recall.js
@@ -7,9 +7,13 @@ export default {
   },
   header: {
     headerText: 'Recall archive into a selected directory',
-    intro: 'You are about to recall an archive – this operation will copy archive contents to selected location. Note that recalling will take some time depending on the size of the archive ({{filesCount}} files, {{size}}) and you will be able to watch recalling progress in file browser.',
+    intro: 'You are about to recall an archive – this operation will copy archive contents to selected location. Note that recalling will take some time depending on the size of the archive ({{filesCount}} {{filesText}}, {{size}}) and you will be able to watch recalling progress in file browser.',
     unknownNumberOf: 'unknown number of',
     unknownSize: 'unknown size',
+    filesText: {
+      singular: 'file',
+      plural: 'files',
+    },
   },
   footer: {
     fileType: {
