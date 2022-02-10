561e35002f8e8f8efe4aa4f9aa0c543200ef0819
Jakub Liput <jakub.liput@gmail.com>
2022-02-10 11:20:40 +0100

    VFS-7717 Using transpilations for browsers since 2017

--- src/config/targets.js
+++ src/config/targets.js
@@ -1,7 +1,7 @@
 /* eslint-env node */
 module.exports = {
   browsers: [
-    'since 2019',
+    'since 2017',
     // must be specified, due to potential bug in build chain: only old versions
     // of android are listed in Babel's data, so it will always add plugins for Android 4
     'not android > 4',
