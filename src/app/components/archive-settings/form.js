f0bf8dec02735c23bc22f0d9bfb799337c77b2ca
Jakub Liput <jakub.liput@gmail.com>
2022-02-21 09:42:49 +0100

    VFS-8654 Added and styles tooltips for archive create

--- src/app/components/archive-settings/form.js
+++ src/app/components/archive-settings/form.js
@@ -147,11 +147,13 @@ export default Component.extend(I18n, {
         { value: 'plain' },
         { value: 'bagit' },
       ],
+      tooltipClass: 'tooltip-lg',
     });
 
     const configNestedField = ToggleField.create({
       name: 'createNestedArchives',
       defaultValue: false,
+      tooltipClass: 'tooltip-lg',
     });
 
     const configIncrementalField = ToggleField
@@ -169,16 +171,19 @@ export default Component.extend(I18n, {
         component: this,
         defaultValue: isBaseArchiveProvided,
         isEnabled: !isBaseArchiveProvided,
+        tooltipClass: 'tooltip-lg',
       });
 
     const configDipField = ToggleField.create({
       name: 'includeDip',
       defaultValue: false,
+      tooltipClass: 'tooltip-lg',
     });
 
     const configSymlinksField = ToggleField.create({
       name: 'followSymlinks',
       defaultValue: true,
+      tooltipClass: 'tooltip-lg',
     });
 
     const configFields = [
