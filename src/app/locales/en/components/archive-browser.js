ac7d71440fced64b9501dfc3bf7c11cf42b7a4e9
Jakub Liput <jakub.liput@gmail.com>
2022-03-16 09:50:50 +0100

    VFS-9102 Fixed secondary mobile info of archive

--- src/app/locales/en/components/archive-browser.js
+++ src/app/locales/en/components/archive-browser.js
@@ -1,6 +1,5 @@
 import tableRow from './archive-browser/table-row';
 import tableRowColumns from './archive-browser/table-row-columns';
-import tableRowMobileSecondaryInfo from './archive-browser/table-row-mobile-secondary-info';
 import tableRowStatusBar from './archive-browser/table-row-status-bar';
 import tableHeadRow from './archive-browser/table-head-row';
 import emptyDir from './archive-browser/empty-dir';
@@ -8,7 +7,6 @@ import emptyDir from './archive-browser/empty-dir';
 export default {
   tableRow,
   tableRowColumns,
-  tableRowMobileSecondaryInfo,
   tableRowStatusBar,
   tableHeadRow,
   emptyDir,
