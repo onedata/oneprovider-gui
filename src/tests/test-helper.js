6a06795abaa5e5d0cb76222cfe710e603a862bee
Jakub Liput <jakub.liput@gmail.com>
2022-04-05 16:05:05 +0200

    VFS-7135 Using common test-helper.js

--- src/tests/test-helper.js
+++ src/tests/test-helper.js
@@ -1,9 +1,1 @@
-import resolver from './helpers/resolver';
-import { setResolver } from 'ember-mocha';
-import { mocha } from 'mocha';
-
-setResolver(resolver);
-
-mocha.setup({
-  timeout: 15000,
-});
+../../onedata-gui-utils/project-common/tests/test-helper.js
