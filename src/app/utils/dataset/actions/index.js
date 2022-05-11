9b9d7786e959c5f13a9fdcee75031ad75d2a33a5
Jakub Liput <jakub.liput@gmail.com>
2022-05-11 15:42:38 +0200

    VFS-8914 Clean up

--- src/app/utils/dataset/actions/index.js
+++ src/app/utils/dataset/actions/index.js
@@ -1,3 +1,11 @@
+/**
+ * Export action classes for dataset.
+ *
+ * @author Jakub Liput
+ * @copyright (C) 2022 ACK CYFRONET AGH
+ * @license This software is released under the MIT license cited in 'LICENSE.txt'.
+ */
+
 import CopyDatasetId from 'oneprovider-gui/utils/dataset/actions/copy-dataset-id';
 import CreateArchive from 'oneprovider-gui/utils/dataset/actions/create-archive';
 import ChangeState from 'oneprovider-gui/utils/dataset/actions/change-state';
