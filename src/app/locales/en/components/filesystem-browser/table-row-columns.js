b0401edc2247d51afb8607853f3ebf22684ba392
plgagneska <agawarchol15@gmail.com>
2022-05-18 08:42:19 +0200

    VFS-9121 Resolve PR tasks

--- src/app/locales/en/components/filesystem-browser/table-row-columns.js
+++ src/app/locales/en/components/filesystem-browser/table-row-columns.js
@@ -3,8 +3,7 @@ export default {
   move: 'moving',
   noSizeInfo: 'Directory size statistics are disabled for this space',
   initializingSizeInfo: 'Requested directory statistics are not ready yet – calculation is in progress.',
-  initializingStatsInfo: 'Directory size statistics are initializing',
+  initializingStatsInfo: 'Directory size statistics are being initialized',
   enabledStatsInfo: 'Show more size statistics',
-  stoppingStatsInfo: 'Directory size statistics are stopping',
   disabledStatsInfo: 'Directory size statistics are disabled',
 };
