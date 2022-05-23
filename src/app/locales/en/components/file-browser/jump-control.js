458f88c2f77d760653c34957a4e2b3483df6c528
Jakub Liput <jakub.liput@gmail.com>
2022-05-23 14:08:40 +0200

    VFS-9235 Changed design/text of jump-control

--- src/app/locales/en/components/file-browser/jump-control.js
+++ src/app/locales/en/components/file-browser/jump-control.js
@@ -1,3 +1,4 @@
 export default {
   jumpToPrefix: 'Jump to prefix...',
+  tip: 'Enter a prefix of item name in current directory to scroll to it in the browser. Note that the search string is case-sensitive. If there is no item with given prefix, you will be scrolled to the nearest matching item.',
 };
