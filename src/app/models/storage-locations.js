2dffd2b4403de0da07805fa60b692f75726d40b7
plgagneska <agawarchol15@gmail.com>
2022-07-20 14:57:39 +0200

    VFS-9472 Add storage location to file model

---
+++ src/app/models/storage-locations.js
@@ -0,0 +1,15 @@
+/**
+ * @module models/storage-locations
+ * @author Agnieszka Warchoł
+ * @copyright (C) 2022 ACK CYFRONET AGH
+ * @license This software is released under the MIT license cited in 'LICENSE.txt'.
+ */
+
+import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
+import StaticGraphModelMixin from 'onedata-gui-websocket-client/mixins/models/static-graph-model';
+import GraphSingleModelMixin from 'onedata-gui-websocket-client/mixins/models/graph-single-model';
+
+export default Model.extend(GraphSingleModelMixin, {
+  locationsPerStorage: attr('object'),
+}).reopenClass(StaticGraphModelMixin);
