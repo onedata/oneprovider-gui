8581fd2c08f409d0d543add56194b7be1fa703a2
Jakub Liput <jakub.liput@gmail.com>
2022-10-08 07:46:52 +0200

    VFS-9614 Clean up and ESLint fixes

--- src/tests/integration/components/archive-properties-test.js
+++ src/tests/integration/components/archive-properties-test.js
@@ -4,7 +4,6 @@ import { setupRenderingTest } from 'ember-mocha';
 import { render, find } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import {
-  getBrowsableArchiveName,
   createArchive,
   createDataset,
 } from '../../helpers/datasets-archives';
