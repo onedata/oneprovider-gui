6ec589252ffaa35cd30a7bed39ebbb01421bed21
Jakub Liput <jakub.liput@gmail.com>
2022-11-18 10:46:14 +0100

    VFS-9786 Added secondary POSIX entities names with tooltips

--- src/app/locales/en/components/posix-permissions-editor.js
+++ src/app/locales/en/components/posix-permissions-editor.js
@@ -4,6 +4,16 @@ export default {
     group: 'Group',
     other: 'Other',
   },
+  entitiesSecondary: {
+    user: 'owner',
+    group: 'space members',
+    other: 'anonyous',
+  },
+  entitiesTip: {
+    user: 'Permissions for the owner of the file. For new files, the creator becomes the owner.',
+    group: 'Permissions for all space members, applied for users other than the owner.',
+    other: 'Permissions for anonymous guests accessing the file via a public share (imposes read-only mode; the write permission is always ignored).',
+  },
   permissions: {
     read: 'Read',
     write: 'Write',
