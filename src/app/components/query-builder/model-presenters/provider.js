ad6bf1c23c25e4dec0c5bcc4a9349b8b2e69ccc2
Jakub Liput <jakub.liput@gmail.com>
2023-01-24 11:33:05 +0100

    VFS-10312 Changed name conflict disambiguation char to "@"

--- src/app/components/query-builder/model-presenters/provider.js
+++ src/app/components/query-builder/model-presenters/provider.js
@@ -1,15 +1,23 @@
 /**
  * Representation of provider
- * 
+ *
  * @module components/query-builder/model-presenters/provider
  * @author Jakub Liput
  * @copyright (C) 2021 ACK CYFRONET AGH
  * @license This software is released under the MIT license cited in 'LICENSE.txt'.
  */
 
+import { computed } from '@ember/object';
 import GenericModelPresenter from './-generic';
+import {
+  defaultSeparator as defaultNameConflictSeparator,
+} from 'onedata-gui-common/components/name-conflict';
 import layout from '../../../templates/components/query-builder/model-presenters/provider';
 
 export default GenericModelPresenter.extend({
   layout,
+
+  nameConflictSeparator: computed(function nameConflictSeparator() {
+    return ` ${defaultNameConflictSeparator}`;
+  }),
 });
