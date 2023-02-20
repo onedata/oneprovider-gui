17f438ab5ab50d16fea1dca2e20c52c43c6924de
Jakub Liput <jakub.liput@gmail.com>
2023-02-20 14:47:15 +0100

    VFS-10388 PR fixes

--- src/app/components/dataset-protection/dataset-item.js
+++ src/app/components/dataset-protection/dataset-item.js
@@ -8,7 +8,7 @@
  */
 
 import Component from '@ember/component';
-import { get } from '@ember/object';
+import EmberObject, { get, computed } from '@ember/object';
 import { reads } from '@ember/object/computed';
 import { and, conditional, raw, not, or } from 'ember-awesome-macros';
 import notImplementedIgnore from 'onedata-gui-common/utils/not-implemented-ignore';
@@ -16,6 +16,7 @@ import { inject as service } from '@ember/service';
 import I18n from 'onedata-gui-common/mixins/components/i18n';
 import protectionIcons from 'oneprovider-gui/utils/dataset-protection/protection-icons';
 import computedT from 'onedata-gui-common/utils/computed-t';
+import OwnerInjector from 'onedata-gui-common/mixins/owner-injector';
 
 export default Component.extend(I18n, {
   tagName: 'tr',
@@ -59,19 +60,13 @@ export default Component.extend(I18n, {
    * @virtual optional
    * @type {String}
    */
-  togglesReadonlyMessage: '',
+  togglesReadonlyMessage: 'Test 9',
 
   /**
    * @virtual
    * @type {Function}
    */
   updateOpenedFileData: notImplementedIgnore,
-
-  /**
-   * Mapping of protection type (data or metadata) to name of icon representing it
-   * @type {Object}
-   */
-  protectionIcons,
 
   /**
    * @type {ComputedProeprty<string>}
@@ -82,10 +77,6 @@ export default Component.extend(I18n, {
   ),
 
   dataEffToggleReadonlyMessage: reads('dataToggleReadonlyMessage'),
-
-  dataReadonly: or('readonly', 'dataEffToggleReadonlyMessage'),
-
-  metadataReadonly: or('readonly', 'metadataEffToggleReadonlyMessage'),
 
   /**
    * @type {ComputedProperty<Boolean>}
@@ -108,39 +99,103 @@ export default Component.extend(I18n, {
     'dataset.metadataIsProtected',
   ),
 
-  actions: {
-    async toggleDatasetProtectionFlag(flag, state) {
-      const setProtectionFlags = [];
-      const unsetProtectionFlags = [];
-      // illegal operation - cannot set metadata protection when data is not protected
-      if (flag === 'metadata_protection' && state && !this.dataIsProtected) {
-        return;
-      }
-      (state ? setProtectionFlags : unsetProtectionFlags).push(flag);
-      // auto turn off metadata protection when data protection is set to off
-      if (flag === 'data_protection' && !state) {
-        unsetProtectionFlags.push('metadata_protection');
-      }
-      try {
-        await this.datasetManager.changeMultipleDatasetProtectionFlags(
-          this.dataset,
-          setProtectionFlags,
-          unsetProtectionFlags,
-        );
-      } catch (error) {
-        this.globalNotify.backendError(this.t('changingWriteProtectionSettings'), error);
-        throw error;
-      } finally {
-        // do not wait for resolve - it's only a side effect
-        if (typeof updateOpenedFileData === 'function') {
-          const rootFileProxy = get(this.dataset, 'rootFile');
-          if (rootFileProxy) {
-            rootFileProxy.then(file => {
-              this.updateOpenedFileData({ fileInvokingUpdate: file });
-            });
-          }
+  toggleViewModels: computed(function toggleViewModels() {
+    return [
+      createToggleViewModel(this, 'data'),
+      createToggleViewModel(this, 'metadata'),
+    ];
+  }),
+
+  async toggleDatasetProtectionFlag(flag, state) {
+    const setProtectionFlags = [];
+    const unsetProtectionFlags = [];
+    (state ? setProtectionFlags : unsetProtectionFlags).push(flag);
+    // auto turn off metadata protection when data protection is set to off
+    if (flag === 'data_protection' && !state) {
+      unsetProtectionFlags.push('metadata_protection');
+    }
+    try {
+      await this.datasetManager.changeMultipleDatasetProtectionFlags(
+        this.dataset,
+        setProtectionFlags,
+        unsetProtectionFlags,
+      );
+    } catch (error) {
+      this.globalNotify.backendError(this.t('changingWriteProtectionSettings'), error);
+      throw error;
+    } finally {
+      // do not wait for resolve - it's only a side effect
+      if (typeof updateOpenedFileData === 'function') {
+        const rootFileProxy = get(this.dataset, 'rootFile');
+        if (rootFileProxy) {
+          rootFileProxy.then(file => {
+            this.updateOpenedFileData({ fileInvokingUpdate: file });
+          });
         }
       }
-    },
+    }
   },
 });
+
+function createToggleViewModel(datasetItem, protectionType) {
+  return EmberObject.extend(I18n, OwnerInjector, {
+    i18n: service(),
+
+    /**
+     * @override
+     */
+    i18nPrefix: 'components.datasetProtection.datasetItem',
+
+    /**
+     * @virtual
+     * @type {Components.DatasetProtection.DatasetItem}
+     */
+    datasetItem: undefined,
+
+    /**
+     * @override
+     */
+    ownerSource: reads('datasetItem'),
+
+    protectionType,
+
+    protectionTypeClass: `col-dataset-protection-${protectionType}`,
+
+    flagToggleId: computed('datasetItem.elementId', function flagToggleId() {
+      return `${this.datasetItem.elementId}-${protectionType}-flag-toggle`;
+    }),
+
+    protectionTypeText: computedT(`toggleLabels.${protectionType}`),
+
+    cellProtectionTypeClass: `col-dataset-protection-${protectionType}`,
+
+    flagToggleClass: `flag-toggle ${protectionType}-flag-toggle`,
+
+    isChecked: reads(`datasetItem.${protectionType}IsProtected`),
+
+    effToggleReadonlyMessage: reads(
+      `datasetItem.${protectionType}EffToggleReadonlyMessage`
+    ),
+
+    isReadOnly: or(
+      'datasetItem.readonly',
+      'effToggleReadonlyMessage'
+    ),
+
+    lockHint: or('effToggleReadonlyMessage', 'datasetItem.togglesReadonlyMessage'),
+
+    icon: protectionIcons[protectionType],
+
+    updateAction: computed('datasetItem', function updateAction() {
+      return (state) => {
+        if (this.isReadOnly) {
+          return;
+        }
+        return this.datasetItem.toggleDatasetProtectionFlag(
+          `${protectionType}_protection`,
+          state
+        );
+      };
+    }),
+  }).create({ datasetItem });
+}
