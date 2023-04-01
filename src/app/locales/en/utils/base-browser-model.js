0b0f675be8533d073f37fccb717069c6d461adf3
Jakub Liput <jakub.liput@gmail.com>
2023-04-01 10:01:52 +0200

    VFS-10461 Text update

--- src/app/locales/en/utils/base-browser-model.js
+++ src/app/locales/en/utils/base-browser-model.js
@@ -4,7 +4,7 @@ export default {
     refresh: 'Refresh',
   },
   refreshTip: {
-    enabled: '<p>Up-to-date – the file list is automatically refreshed every {{pollingIntervalSecs}} seconds.</p><p>Use this button to trigger an extra refresh at any time.</p>',
-    selectedDisabled: '<p>You are viewing the file list from {{lastRefreshTime}}. Auto-refresh is disabled as long as your selection contains items falling outside of the displayed range.</p><p>This button will trigger a refresh, but your current selection may be lost.</p>',
+    enabled: '<p>Up-to-date – the list is automatically refreshed every {{pollingIntervalSecs}} seconds.</p><p>Use this button to trigger an extra refresh at any time.</p>',
+    selectedDisabled: '<p>You are viewing the listing from {{lastRefreshTime}}. Auto-refresh is disabled as long as your selection contains items falling outside of the displayed range.</p><p>This button will trigger a refresh, but your current selection may be lost.</p>',
   },
 };
