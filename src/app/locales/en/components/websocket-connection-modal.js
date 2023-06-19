a638b4eca0ec5a097a3acadbc6382fbaf35a6d34
Jakub Liput <jakub.liput@gmail.com>
2023-06-19 12:58:27 +0200

    VFS-10962 Using generic websocket-reconnection-modal

--- src/app/locales/en/components/websocket-connection-modal.js
+++
@@ -1,5 +0,0 @@
-export default {
-  header: 'Connection lost',
-  content: 'WebSocket connection to Oneprovider has been closed unexpectedly, try using the reload button below or refresh whole application.',
-  reload: 'Reload',
-};
