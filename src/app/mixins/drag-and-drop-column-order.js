a49ee1f2d46e54cd301d9a85045b13528b9c4ba6
plgagneska <agawarchol15@gmail.com>
2023-11-08 15:04:06 +0100

    VFS-11405 Resolve PR tasks

--- src/app/mixins/drag-and-drop-column-order.js
+++ src/app/mixins/drag-and-drop-column-order.js
@@ -1,5 +1,36 @@
 /**
- * Utils to change order of columns using drag and drop actions.
+ * Utils to change the order of columns using drag and drop actions.
+ * To use that add to table's headers in hbs draggable event handlers:
+ *     - ondragstart with headingDragAction action assigned to it and pass column name as an argument
+ *     - ondragend with headingDragEndAction action assigned to it
+ * Add also div element which creates a draggable area, where headers can be dropped,
+ * and to that element add event handlers:
+ *     - ondragover with headingDragOverAction action assigned to it
+ *     - ondragleave with headingDragLeaveAction action assigned to it
+ *     - ondrop with headingDropAction action assigned to it and pass an index of column,
+ * in some of the places the first column is not included in the list of columns,
+ * in such case, this index should be inclement by 1.
+ * Add drag-and-drop-column-order class to table, to properly show borders and drag icon in headers.
+ *
+ * Example:
+ * ```
+ * <table class="drag-and-drop-column-order">
+ *  <thead>
+ *    <tr>
+ *      <th
+ *        ondragstart={{action "headingDragAction" columnName}}
+ *        ondragend={{action "headingDragEndAction"}}
+ *      >
+ *        ...
+ *        <div
+ *          ondragover={{action "headingDragOverAction"}}
+ *          ondragleave={{action "headingDragLeaveAction"}}
+ *          ondrop={{action "headingDropAction" i}}
+ *        >
+ *        </div>
+ *     </th>
+ *      ...
+ * ```
  *
  * @author Agnieszka Warchoł
  * @copyright (C) 2023 ACK CYFRONET AGH
@@ -54,7 +85,7 @@ export default Mixin.create({
       this.set('lastActiveDropOverElem', lastActiveDropOverElem);
     },
     headingDragLeaveAction() {
-      this.lastActiveDropOverElem.classList.remove('border-solid');
+      this.lastActiveDropOverElem?.classList.remove('border-solid');
     },
   },
 });
