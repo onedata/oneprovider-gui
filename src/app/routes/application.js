908f7e83eb1af1880e1a6f321f861299a39bdee6
Jakub Liput <jakub.liput@gmail.com>
2024-01-25 18:44:51 +0100

    VFS-11513 Fixed ESLint issues

--- src/app/routes/application.js
+++ src/app/routes/application.js
@@ -10,7 +10,6 @@ import { inject as service } from '@ember/service';
 import OnedataApplicationRoute from 'onedata-gui-common/routes/application';
 import DevelopmentModelRouteMixin from 'onedata-gui-websocket-client/mixins/routes/development-model';
 import UnifiedGuiController from 'onedata-gui-common/utils/unified-gui-controller';
-import globals from 'onedata-gui-common/utils/globals';
 
 export default OnedataApplicationRoute.extend(DevelopmentModelRouteMixin, {
   onedataWebsocket: service(),
