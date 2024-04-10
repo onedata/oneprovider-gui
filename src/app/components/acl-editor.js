dc06552b2cbb9970b257910a2c3410a607c95ced
Jakub Liput <jakub.liput@gmail.com>
2024-04-10 10:41:04 +0200

    VFS-11801 Auto expand and focus ACE on the list on ACE add

--- src/app/components/acl-editor.js
+++ src/app/components/acl-editor.js
@@ -13,6 +13,8 @@ import I18n from 'onedata-gui-common/mixins/i18n';
 import { array } from 'ember-awesome-macros';
 import { inject as service } from '@ember/service';
 import { AceFlagsMasks } from 'oneprovider-gui/utils/acl-permissions-specification';
+import { guidFor } from '@ember/object/internals';
+import sleep from 'onedata-gui-common/utils/sleep';
 
 export default Component.extend(I18n, {
   classNames: ['acl-editor'],
@@ -89,6 +91,14 @@ export default Component.extend(I18n, {
    */
   subjectsList: array.concat('sortedGroupsAndUsers', 'systemSubjects'),
 
+  aceListItems: computed('acl.[]', function aceListItems() {
+    const x = this.acl?.map(ace => ({
+      id: guidFor(ace),
+      ace,
+    })) ?? [];
+    return x;
+  }),
+
   /**
    * @returns {undefined}
    */
@@ -98,6 +108,47 @@ export default Component.extend(I18n, {
       onChange,
     } = this.getProperties('acl', 'onChange');
     onChange(acl);
+  },
+
+  addEntity(entity) {
+    const modelName = get(entity, 'constructor.modelName');
+    const subjectType = modelName || get(entity, 'equivalentType') || 'group';
+    const aceFlags = get(entity, 'constructor.modelName') === 'group' ?
+      AceFlagsMasks.IDENTIFIER_GROUP : AceFlagsMasks.NO_FLAGS;
+
+    const newAce = {
+      aceMask: 0,
+      aceType: 'ALLOW',
+      identifier: get(entity, 'entityId'),
+      aceFlags,
+      subject: entity,
+      subjectType,
+    };
+    this.get('acl').pushObject(newAce);
+    (async () => {
+      // await waitForRender();
+      await sleep(0);
+      this.expandAce(guidFor(newAce));
+    })();
+    this.notifyAboutChange();
+  },
+
+  expandAce(aceId) {
+    const aceItemElement = this.element?.querySelector(`#ace-item-${aceId}`);
+    if (!aceItemElement) {
+      console.warn('AclEditor: New ACE item not found on list');
+    }
+
+    aceItemElement
+      ?.querySelector('.one-collapsible-list-item-header')
+      ?.click();
+
+    const onTransitionEnd = () => {
+      aceItemElement.scrollIntoView();
+      aceItemElement.removeEventListener('transitionend', onTransitionEnd);
+    };
+
+    aceItemElement.addEventListener('transitionend', onTransitionEnd);
   },
 
   actions: {
@@ -132,21 +183,7 @@ export default Component.extend(I18n, {
       this.notifyAboutChange();
     },
     addEntitySelected(entity) {
-      const modelName = get(entity, 'constructor.modelName');
-      const subjectType = modelName || get(entity, 'equivalentType') || 'group';
-      const aceFlags = get(entity, 'constructor.modelName') === 'group' ?
-        AceFlagsMasks.IDENTIFIER_GROUP : AceFlagsMasks.NO_FLAGS;
-
-      const newAce = {
-        aceMask: 0,
-        aceType: 'ALLOW',
-        identifier: get(entity, 'entityId'),
-        aceFlags,
-        subject: entity,
-        subjectType,
-      };
-      this.get('acl').pushObject(newAce);
-      this.notifyAboutChange();
+      this.addEntity(entity);
     },
   },
 });
