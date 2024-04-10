e4381ac7fc8f69e2f9c4759491208893da818ae7
Jakub Liput <jakub.liput@gmail.com>
2024-04-10 13:45:41 +0200

    VFS-11801 Added warning about no rules in ACE

---
+++ src/app/locales/en/components/acl-editor/ace-editor.js
@@ -0,0 +1,23 @@
+export default {
+  aceType: 'Type',
+  aceTypes: {
+    allow: 'Allow',
+    deny: 'Deny',
+  },
+  acePermissionState: {
+    deny: 'denied',
+    allow: 'allowed',
+  },
+  moveDown: 'Move down',
+  moveUp: 'Move up',
+  remove: 'Remove',
+  unknown: 'Unknown',
+  id: 'ID',
+  aceSubjects: {
+    user: 'user',
+    group: 'group',
+  },
+  aceNotAccessible: 'This Access Control Entry refers to a {{subject}} that is no longer a member of this space.',
+  noRulesEnabled: 'No rules enabled',
+  noRulesEnabledTip: 'This set of rules effectively <strong>denies</strong> everything for the selected entity.',
+};
