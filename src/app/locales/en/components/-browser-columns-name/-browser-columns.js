90ba9c6c7b24222dd31bbd2189be3c61ecf91864
plgagneska <agawarchol15@gmail.com>
2024-04-11 14:45:59 +0200

    VFS-11536 Fix tests

--- src/app/locales/en/components/-browser-columns-name/-browser-columns.js
+++ src/app/locales/en/components/-browser-columns-name/-browser-columns.js
@@ -12,7 +12,7 @@ export default {
   tip: {
     qos: 'The aggregated fulfillment status of the imposed Quality of Service requirements.',
     replication: 'The ratio of file/directory data stored on the current Oneprovider\'s (<strong>{{oneprovider}}</strong>) storage systems compared to its logical size.',
-    modification: 'Last time the item\'s content was modified.For files, indicates the time of the last write or truncate operation on the file. For directories, updated when a child item is added, renamed or deleted.',
+    modification: 'Last time the item\'s content was modified. For files, indicates the time of the last write or truncate operation on the file. For directories, updated when a child item is added, renamed or deleted.',
     atime: 'Last time the item was accessed.',
     ctime: 'Last time the item\'s metadata was changed, e.g. permissions. It\' s also always updated when the modification time changes.',
   },
