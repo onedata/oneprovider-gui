6c29f204b1fe972a6480e41f19e494b49f124ded
Jakub Liput <jakub.liput@gmail.com>
2024-04-11 09:31:09 +0200

    VFS-11761 Updated texts for share-modal

--- src/app/locales/en/components/modals/share-modal.js
+++ src/app/locales/en/components/modals/share-modal.js
@@ -3,11 +3,12 @@ import { noHandleServicesText } from '../share-show/pane-opendata';
 export default {
   header: 'Share / Publish {{fileType}}',
   cancel: 'Cancel',
-  setName: 'Enter name for created share that will be visible to other users',
+  intro: 'Allows read-only access for anyone who knows the share link.',
+  shareNameIntro: 'Share name (visible to the share audience):',
   creatingShare: 'creating share',
   createNew: 'Create',
-  publishText: 'I want to publish this shared dataset as Open Data',
-  publishTip: '<p>You can register this shared dataset in a handle service – it will be assigned a persistent identifier (e.g. PID or DOI) and exposed for discovery by Open Data indexes via OAI PMH protocol. This process will make your dataset globally available (without an account in Onedata) and anyone will be able to search for it in the Open Data indexes.</p><p>By selecting this option, you will be directed to the Open Data wizard.</p>',
+  publishText: 'Publish as an Open Data record',
+  publishTip: 'Choose this option to create a share and publish it as an Open Data record at once. The shared data collection will be registered in a handle service; assigned a persistent identifier (e.g. PID or DOI) and exposed for discovery by Open Data indexes via OAI PMH protocol. This process will make your data collection publicly available (without an account in Onedata) and anyone will be able to look it up in the Open Data indexes.',
   publishImpossibleTip: noHandleServicesText,
   validations: {
     nameTooShort: 'Name must have at least {{length}} characters',
--- src/app/locales/en/components/share-show/pane-opendata.js
+++ src/app/locales/en/components/share-show/pane-opendata.js
@@ -1,5 +1,5 @@
 export const noHandleServicesText =
-  'You do not have access to any handle service, which is necessary to register Open Data handles. You can either create a new handle service (requires <code>oz_handle_services_create</code> admin privilege), or contact the site administrators and ask for access to an existing handle service.';
+  'You do not have access to any handle service, which is necessary to register Open Data records. You can either create a new handle service (requires <code>oz_handle_services_create</code> admin privilege) or request access to an existing handle service from the site administrators.';
 
 export default {
   publishWelcome: {
