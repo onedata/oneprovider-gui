5a93c2ea71a86dc7c5edb1882d5ecb23bed0834d
Jakub Liput <jakub.liput@gmail.com>
2024-04-11 10:02:59 +0200

    VFS-11801 Text changes; default dummy component settings

--- src/app/locales/en/components/acl-editor/ace-editor.js
+++ src/app/locales/en/components/acl-editor/ace-editor.js
@@ -19,5 +19,6 @@ export default {
   },
   aceNotAccessible: 'This Access Control Entry refers to a {{subject}} that is no longer a member of this space.',
   noRulesEnabled: 'Select at least one operation',
+  noRulesEnabledReadonly: 'This entry has no effect',
   noRulesEnabledTip: 'An entry that does not allow or deny anything is pointless — it will not impact the permissions for this principal in any way.',
 };
