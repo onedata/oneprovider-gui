98d2daccd58437f221dbc54d7a4def675d5e3e27
Jakub Liput <jakub.liput@gmail.com>
2024-12-13 11:08:47 +0100

    VFS-12267 Special Oneprovider message for service unavailable error

---
+++ src/app/locales/en/components/errors/service-temporarily-unavailable.js
@@ -0,0 +1,4 @@
+export default {
+  header: 'Service temporarily unavailable',
+  text: '<p>You may try reloading the page.<br>If the problem persists, contact the service administrator.</p><p>To access the space, consider switching to another provider (if available) using the top bar.</p>',
+};
