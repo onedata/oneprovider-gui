62a60650ce19d2d9608bc158e3133b3c767b7bfb
plgagneska <agawarchol15@gmail.com>
2025-03-03 15:02:27 +0100

    VFS-12259 Changed tooltips for json columns

--- src/app/locales/en/components/-browser-columns-name/filesystem.js
+++ src/app/locales/en/components/-browser-columns-name/filesystem.js
@@ -23,8 +23,8 @@ export default {
     posixPermissions: 'Permissions regulating data access rights. It can be expressed either by the standard POSIX permissions system or by the Access Control List (ACL). If there is an ACL specified, the POSIX permissions are ignored.<br>Click on the cell to see more details.',
     xattr: 'The extended attribute value for key: <strong>{{key}}</strong>',
     json: {
-      all: 'Custom JSON metadata assigned to the file. Showing the whole JSON metadata.',
-      key: 'Custom JSON metadata assigned to the file. Showing only value of key: <strong>{{key}}</strong>.',
+      all: 'Custom JSON metadata assigned to the file (mode: whole document).',
+      key: 'Custom JSON metadata assigned to the file (mode: extract key <strong>"{{key}}"</strong>).',
     },
     fileId: {
       priv: 'File ID is a unique, global identifier associated with a file/directory. It can be used to access and interact with the file/directory using different interfaces (e.g. REST API), but only by authorized users that have permission to do so.',
