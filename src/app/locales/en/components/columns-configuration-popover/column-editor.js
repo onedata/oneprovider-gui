e6bbddd3e43d3f37cb509e8a006384a0573c62b9
plgagneska <agawarchol15@gmail.com>
2025-03-28 16:07:41 +0100

    VFS-12606 Fixed styles

--- src/app/locales/en/components/columns-configuration-popover/column-editor.js
+++ src/app/locales/en/components/columns-configuration-popover/column-editor.js
@@ -12,7 +12,7 @@ export default {
   columnLabelExistsTooltip: 'Column with this label already exists.',
   xattrKey: {
     label: 'Extended attribute key',
-    customValueInputPlaceholder: 'Enter an xattr key...',
+    customValueInputPlaceholder: 'Enter or choose a key...',
   },
   metadataType: {
     label: 'Metadata type',
