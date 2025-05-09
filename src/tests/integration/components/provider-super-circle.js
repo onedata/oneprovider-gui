f10317058e73aba5b2fa5ea8ecf2bbd9865d9131
Jakub Liput <jakub.liput@gmail.com>
2025-05-09 13:57:03 +0200

    VFS-12765 Added <ProviderSuperCircle> tests; updated onedata-gui-common

---
+++ src/tests/integration/components/provider-super-circle.js
@@ -0,0 +1,49 @@
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+import { setupRenderingTest } from 'ember-mocha';
+import { render, find } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+describe('Integration | Component | provider-place/super-circle', function () {
+  setupRenderingTest();
+
+  it('renders with source and without desitnation animation class', async function () {
+    await render(hbs `<ProviderSuperCircle @isSource={{true}} />`);
+
+    const superCircle = find('.super-circle');
+
+    expect(superCircle).to.have.class('source');
+    expect(superCircle).to.not.have.class('destination');
+  });
+
+  it('renders with destination and without source animation class', async function () {
+    await render(hbs `<ProviderSuperCircle @isDestination={{true}} />`);
+
+    const superCircle = find('.super-circle');
+
+    expect(superCircle).to.not.have.class('source');
+    expect(superCircle).to.have.class('destination');
+  });
+
+  it('renders with source and destination animation classes', async function () {
+    await render(hbs `
+      <ProviderSuperCircle @isDestination={{true}} @isSource={{true}} />
+    `);
+
+    const superCircle = find('.super-circle');
+
+    expect(superCircle).to.have.class('source');
+    expect(superCircle).to.have.class('destination');
+  });
+
+  it('renders without source and destination animation classes', async function () {
+    await render(hbs `
+      <ProviderSuperCircle @isDestination={{false}} @isSource={{false}} />
+    `);
+
+    const superCircle = find('.super-circle');
+
+    expect(superCircle).to.not.have.class('source');
+    expect(superCircle).to.not.have.class('destination');
+  });
+});
