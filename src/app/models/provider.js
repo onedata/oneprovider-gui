9299b8d1403d42c96dccb9a6ffec4cbe95e67c83
Jakub Liput <jakub.liput@gmail.com>
2025-12-04 13:43:20 +0100

    VFS-12892 Added releaseVersion alias in provider model

--- src/app/models/provider.js
+++ src/app/models/provider.js
@@ -1,6 +1,7 @@
 /**
- * @author Michał Borzęcki
+ * @author Michał Borzęcki, Jakub Liput
  * @copyright (C) 2019 ACK CYFRONET AGH
+ * @copyright (C) 2025 Onedata (onedata.org)
  * @license This software is released under the MIT license cited in 'LICENSE.txt'.
  */
 
@@ -8,6 +9,7 @@ import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
 import StaticGraphModelMixin from 'onedata-gui-websocket-client/mixins/models/static-graph-model';
 import GraphSingleModelMixin from 'onedata-gui-websocket-client/mixins/models/graph-single-model';
+import { reads } from '@ember/object/computed';
 
 export const entityType = 'op_provider';
 
@@ -18,4 +20,10 @@ export default Model.extend(GraphSingleModelMixin, {
   online: attr('boolean'),
   domain: attr('string'),
   version: attr('string'),
+
+  /**
+   * Release version, which is currently stored in `version` attribute.
+   * For compatibility with Onezone Provider model.
+   */
+  releaseVersion: reads('string'),
 }).reopenClass(StaticGraphModelMixin);
