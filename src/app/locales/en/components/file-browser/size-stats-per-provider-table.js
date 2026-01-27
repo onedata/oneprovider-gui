ece141c6fc7dbd3155aec319bb1823e32fff6bd6
plgagneska <agawarchol15@gmail.com>
2026-01-27 12:30:28 +0100

    VFS-13031 Fixed texts

--- src/app/locales/en/components/file-browser/size-stats-per-provider-table.js
+++ src/app/locales/en/components/file-browser/size-stats-per-provider-table.js
@@ -7,7 +7,7 @@ export default {
   containsTip: 'The values may be different for each provider due to delays in synchronization of recent data modifications.',
   logicalSizeTip: 'Logical size is the total size of file data contained in this directory, i.e. the sum of logical byte sizes of all regular files. <br> ' +
     synchronizationDelay,
-  physicalSizeTip: 'Physical size is the actual amount of storage consumed on a given storage backend by all regular files in the directory\'s subtree.May be smaller than the virtual size if some file replicas are incomplete. <br> ' +
+  physicalSizeTip: 'Physical size is the actual amount of storage consumed on a given storage backend by all regular files in the directory\'s subtree. May be smaller than the virtual size if some file replicas are incomplete. <br> ' +
     synchronizationDelay,
   virtualSizeTip: 'Virtual size is the logical size with hardlinks counted only once, i.e. the size of unique file data in the subtree. Represents the storage space required for a complete replica of the directory. <br> ' +
     synchronizationDelay,
